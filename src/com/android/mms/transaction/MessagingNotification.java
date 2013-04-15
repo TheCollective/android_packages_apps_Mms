61acd4012fdf0cd5497f301ed7de217e66c17f5d
fezzy <fezzy@fezzy-VirtualBox.(none)>
2013-04-15 12:39:55 -0400

    Merge branch 'cm-10.1' of git://github.com/CyanogenMod/android_packages_apps_Mms into jellybean2
    Conflicts:
    	res/values-da/strings.xml
    	res/values-de/strings.xml
    	res/values-el/strings.xml
    	res/values-es/strings.xml
    	res/values-fr/strings.xml
    	res/values-hu/strings.xml
    	res/values-it/strings.xml
    	res/values-iw/strings.xml
    	res/values-nl/strings.xml
    	res/values-pl/strings.xml
    	res/values-pt/strings.xml
    	res/values-ru/strings.xml
    	res/values-zh-rCN/strings.xml
    	res/values/strings.xml
    	src/com/android/mms/transaction/MessagingNotification.java
    	src/com/android/mms/util/ThumbnailManager.java
    
    'Merge update into cm-10.1'

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -64,6 +64,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.android.mms.LogTag;
+import com.android.mms.MmsConfig;
 import com.android.mms.R;
 import com.android.mms.data.Contact;
 import com.android.mms.data.Conversation;
@@ -795,6 +796,9 @@ public class MessagingNotification {
             Bitmap attachmentBitmap,
             Contact contact,
             int attachmentType) {
+        if (MmsConfig.isSuppressedSprintVVM(address)) {
+            return null;
+        }
         Intent clickIntent = ComposeMessageActivity.createIntent(context, threadId);
         clickIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                 | Intent.FLAG_ACTIVITY_SINGLE_TOP
@@ -806,7 +810,6 @@ public class MessagingNotification {
                 0, senderInfo.length() - 2);
         CharSequence ticker = buildTickerMessage(
                 context, address, subject, message);
-
         return new NotificationInfo(isSms,
                 clickIntent, message, subject, ticker, timeMillis,
                 senderInfoName, attachmentBitmap, contact, attachmentType, threadId);
@@ -1052,7 +1055,7 @@ public class MessagingNotification {
                 mrIntent.putExtra(QmMarkRead.SMS_THREAD_ID, mostRecentNotification.mThreadId);
                 PendingIntent mrPendingIntent = PendingIntent.getBroadcast(context, 0, mrIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
-                noti.addAction(R.drawable.ic_menu_done_holo_dark, markReadText, mrPendingIntent);
+                noti.addAction(R.drawable.ic_mark_read_holo_dark, markReadText, mrPendingIntent);
 
                 // Add the Call action
                 CharSequence callText = context.getText(R.string.menu_call);
